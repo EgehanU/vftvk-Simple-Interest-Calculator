3a214eb352b131b6a04801a6a4f2b2e8beba803e
Egehan Usluer <egehanusluer@gmail.com>
2022-04-18 02:56:24 +0200

    Change for alert message

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ function compute()
     let rate = document.getElementById("interestRate").value;
     let years = document.getElementById("years").value;
     years = Number.parseFloat(years);
-    if (amount != 0) {
+    if (amount > 0) {
       let targetYear = 2022 + years;
       let gainedAmount = (amount * (rate / 100)) * years;
       document.getElementById("result").innerHTML = ("If you deposit " + amount + ",\nat an interest rate of " + rate + "%.\nYou will receive an amount of " + gainedAmount + ",\nin year "+ targetYear);
 
     }
-    else if (amount == 0) {
-      alert("Either no value has been entered or an entry of zero has been made!")
+    else if (amount <= 0) {
+      alert("Either no value has been entered or an entry of zero has been made, or a negative number has been used! Please enter a positive number")
     }
 
 }
